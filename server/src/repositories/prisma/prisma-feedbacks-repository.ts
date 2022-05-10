11b738fdbb65998c5c14adf04d83d850fbe30359
Lucas Gabriel Gonçalves <lucas.gabriel.goncalves12@gmail.com>
2022-05-10 18:59:43 -0300

    Created counter to count feedbacks types

--- server/src/repositories/prisma/prisma-feedbacks-repository.ts
+++ server/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -16,7 +16,27 @@ export class PrismaFeedbacksRepository implements FeedbacksRepository {
   }
 
   async index() {
-    const feedbacks = await prisma.feedback.findMany({});
-    return feedbacks;
+    const bugs = await prisma.feedback.findMany({
+      where: {
+        type: "BUG",
+      },
+    });
+    const ideas = await prisma.feedback.findMany({
+      where: {
+        type: "IDEA",
+      },
+    });
+    const others = await prisma.feedback.findMany({
+      where: {
+        type: "OTHER",
+      },
+    });
+
+    const result = {
+      BUG: bugs.length ?? 0,
+      IDEA: ideas.length ?? 0,
+      OTHER: others.length ?? 0,
+    };
+    return result;
   }
 }
