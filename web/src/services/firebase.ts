8890b7b8518a6db4e79c3e4250ac61f71483eee7
Lucas Gabriel Gonçalves <lucas.gabriel.goncalves12@gmail.com>
2022-05-10 20:12:52 -0300

    Created variables firebase .env

--- web/src/services/firebase.ts
+++ web/src/services/firebase.ts
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3Fi1GOaZgPFzEgL1uB4-bUBz0fSSLu4g",
-  authDomain: "feedwidget-lg13.firebaseapp.com",
-  projectId: "feedwidget-lg13",
-  storageBucket: "feedwidget-lg13.appspot.com",
-  messagingSenderId: "551070414475",
-  appId: "1:551070414475:web:831210623d4ca2f2cac68e",
-  measurementId: "G-097W506LR6",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET_ID,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
